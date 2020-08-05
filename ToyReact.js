ece44fbd81c0424ab7bdf61d249cf472bcd19e5b
kaelzhu <zljmms32@gmail.com>
2020-08-05 14:42:59 +0800

    实现setState

--- ToyReact.js
+++ ToyReact.js
@@ -1,17 +1,56 @@
 export class Component {
 	constructor() {
 		this.children = []
+		this.props = Object.create(null)
 	}
+
 	setAttribute(name, value) {
+		this.props[name] = value
 		this[name] = value
 	}
 
-	mountTo(parent) {
-		let vdom = this.render()
-		vdom.mountTo(parent)
+	mountTo(range) {
+		this.range = range
+		this.update()
 	}
+
+	update() {
+		let placeholder = document.createComment('placeholder')
+		//  range 是 虚实dom比对的关键
+		let range = document.createRange()
+		range.setStart(this.range.endContainer, this.range.endOffset)
+		range.setEnd(this.range.endContainer, this.range.endOffset)
+		range.insertNode(placeholder)
+
+		this.range.deleteContents()
+		let vdom = this.render()
+		vdom.mountTo(this.range)
+	}
+
 	appendChild(vchild) {
 		this.children.push(vchild)
+	}
+
+	setState(state) {
+		let merge = (oldState, newState) => {
+			for (let p in newState) {
+				if (typeof newState[p] === 'object') {
+					if (typeof oldState[p] !== 'object') {
+						// deep copy
+						oldState[p] = {}
+					}
+					merge(oldState[p], newState[p])
+				} else {
+					oldState[p] = newState[p]
+				}
+			}
+		}
+
+		if (!this.state && state) {
+			this.state = {}
+		}
+		merge(this.state, state)
+		this.update()
 	}
 }
 
@@ -21,15 +60,31 @@ class ElementWrapper {
 	}
 
 	setAttribute(name, value) {
+		if (name.match(/^on([\s\S]+)$/)) {
+			const eventName = RegExp.$1.replace(/^[\s\S]/, (s) =>
+				s.toLowerCase()
+			)
+			this.root.addEventListener(eventName, value)
+		}
+		if (name === 'className') name = 'class'
 		this.root.setAttribute(name, value)
 	}
 
 	appendChild(vchild) {
-		vchild.mountTo(this.root)
+		let range = document.createRange()
+		if (this.root.children.length) {
+			range.setStartAfter(this.root.lastChild)
+			range.setEndAfter(this.root.lastChild)
+		} else {
+			range.setStart(this.root, 0)
+			range.setEnd(this.root, 0)
+		}
+		vchild.mountTo(range)
 	}
 
-	mountTo(parent) {
-		parent.appendChild(this.root)
+	mountTo(range) {
+		range.deleteContents()
+		range.insertNode(this.root)
 	}
 }
 
@@ -38,12 +93,11 @@ class TextWrapper {
 		this.root = document.createTextNode(content)
 	}
 
-	mountTo(parent) {
-		parent.appendChild(this.root)
+	mountTo(range) {
+		range.deleteContents()
+		range.insertNode(this.root)
 	}
 }
-
-
 
 export const ToyReact = {
 	createElement(type, attributes, ...children) {
@@ -52,22 +106,23 @@ export const ToyReact = {
 
 		let element
 		if (typeof type === 'string') element = new ElementWrapper(type)
-		else element = new type
+		else element = new type()
 
 		for (let name in attributes) {
 			element.setAttribute(name, attributes[name])
 		}
-		let insertChildren = children => {
+		let insertChildren = (children) => {
 			for (let child of children) {
 				if (typeof child === 'object' && child instanceof Array) {
 					insertChildren(child)
 				} else {
-					if (!(child instanceof Component)
-						&& !(child instanceof ElementWrapper)
-						&& !(child instanceof TextWrapper)
-						) 
+					if (
+						!(child instanceof Component) &&
+						!(child instanceof ElementWrapper) &&
+						!(child instanceof TextWrapper)
+					)
 						child = String(child)
-					if (typeof child === 'string') 
+					if (typeof child === 'string')
 						child = new TextWrapper(child)
 
 					element.appendChild(child)
@@ -79,6 +134,15 @@ export const ToyReact = {
 		return element
 	},
 	render(vdom, element) {
-		vdom.mountTo(element)
+		let range = document.createRange()
+		if (element.children.length) {
+			range.setStartAfter(element.lastChild)
+			range.setEndAfter(element.lastChild)
+		} else {
+			range.setStart(element, 0)
+			range.setEnd(element, 0)
+		}
+
+		vdom.mountTo(range)
 	},
 }
